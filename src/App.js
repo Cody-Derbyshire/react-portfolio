1773da175e15573fdbbdfbb2b345ac72080de6e4
cody derbyshire <cody.derbyshire@developersinstitute.co.nz>
2023-05-29 11:39:58 +1200

    added spline object

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Spline from '@splinetool/react-spline';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='spline'>
+        <Spline scene='https://prod.spline.design/9415sx1lBu-BzC3E/scene.splinecode' />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
